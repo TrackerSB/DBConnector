fad9869171b363eef23bd8c7d0dae543b8d30b3c
Stefan Huber <stefan.huber.niedling@outlook.com>
2020-06-07 11:40:07 +0200

    SimpleConnection: Remove unused import

--- src/main/java/bayern/steinbrecher/database/connection/SimpleConnection.java
+++ src/main/java/bayern/steinbrecher/database/connection/SimpleConnection.java
@@ -3,7 +3,6 @@ package bayern.steinbrecher.database.connection;
 import bayern.steinbrecher.database.SupportedDatabases;
 import bayern.steinbrecher.database.connection.credentials.SimpleCredentials;
 
-import java.net.UnknownHostException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
