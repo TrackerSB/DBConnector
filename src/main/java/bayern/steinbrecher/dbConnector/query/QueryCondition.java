ec6556c8b6bbd5fe2455853f91fa9701f4cc3242
Stefan Huber <stefan.huber.niedling@outlook.com>
2020-10-04 12:28:00 +0200

    Avoid too deep subclass hierarchies and duplication of argument conversion

--- src/main/java/bayern/steinbrecher/dbConnector/query/QueryCondition.java
+++ src/main/java/bayern/steinbrecher/dbConnector/query/QueryCondition.java
@@ -12,94 +12,107 @@ import java.util.Optional;
  * @since 0.5
  */
 public abstract class QueryCondition<T> {
-    public static final QueryCondition<String> LIKE = new BinaryStringQueryCondition("LIKE");
-    public static final QueryCondition<Boolean> IS_TRUE = new UnaryPrefixBooleanQueryCondition("");
-    public static final QueryCondition<Boolean> IS_FALSE = new UnaryPrefixBooleanQueryCondition("NOT");
+    public static final QueryCondition<String> LIKE
+            = new BinaryQueryCondition<>(ArgumentConverter.STRING_ARGUMENT_CONVERTER, "LIKE");
+    public static final QueryCondition<Boolean> IS_TRUE
+            = new UnaryPrefixQueryCondition<>(ArgumentConverter.BOOLEAN_ARGUMENT_CONVERTER, "");
+    public static final QueryCondition<Boolean> IS_FALSE
+            = new UnaryPrefixQueryCondition<>(ArgumentConverter.BOOLEAN_ARGUMENT_CONVERTER, "NOT");
 
-    private final Class<T> runtimeGenericTypeProvider;
-    private final Class<Column<T>> runtimeGenericColumnTypeProvider;
-    private final ColumnParser<T> columnParser;
+    private final ArgumentConverter<T> argumentConverter;
 
-    protected QueryCondition(@NotNull Class<T> runtimeGenericTypeProvider,
-                             @NotNull Class<Column<T>> runtimeGenericColumnTypeProvider,
-                             @NotNull ColumnParser<T> columnParser) {
-        this.runtimeGenericTypeProvider = Objects.requireNonNull(runtimeGenericTypeProvider);
-        this.runtimeGenericColumnTypeProvider = Objects.requireNonNull(runtimeGenericColumnTypeProvider);
-        this.columnParser = Objects.requireNonNull(columnParser);
+    protected QueryCondition(@NotNull ArgumentConverter<T> argumentConverter) {
+        this.argumentConverter = argumentConverter;
     }
 
-    @NotNull
-    private Optional<String> convertAsColumnArgument(QueryGenerator queryGenerator, @NotNull Object argument) {
-        String convertedArgument;
-        if (runtimeGenericColumnTypeProvider.isAssignableFrom(argument.getClass())) {
-            convertedArgument = queryGenerator.quoteIdentifier(
-                    runtimeGenericColumnTypeProvider.cast(argument).getName());
-        } else {
-            convertedArgument = null;
-        }
-        return Optional.ofNullable(convertedArgument);
-    }
-
-    @NotNull
-    private Optional<String> convertAsTypeArgument(@NotNull Object argument) {
-        String convertedArgument;
-        if (runtimeGenericTypeProvider.isAssignableFrom(argument.getClass())) {
-            convertedArgument = columnParser.toString(runtimeGenericTypeProvider.cast(argument));
-        } else {
-            convertedArgument = null;
-        }
-        return Optional.ofNullable(convertedArgument);
-    }
-
-    @NotNull
-    protected String convertArgument(@NotNull QueryGenerator queryGenerator, @NotNull Object argument) {
-        return convertAsTypeArgument(argument)
-                .or(() -> convertAsColumnArgument(queryGenerator, argument))
-                .orElseThrow(() -> new IllegalArgumentException(
-                        String.format("Only objects of type '%s' and '%s' as arguments accepted",
-                                runtimeGenericTypeProvider.getSimpleName(), Column.class.getSimpleName())));
+    protected ArgumentConverter<T> getArgumentConverter() {
+        return argumentConverter;
     }
 
     @NotNull
     public abstract String generateSQLExpression(@NotNull QueryGenerator queryGenerator, @NotNull Object... arguments);
 
-    private static abstract class BinaryQueryCondition<T> extends QueryCondition<T> {
+    public static final class ArgumentConverter<T> {
+        public static final ArgumentConverter<String> STRING_ARGUMENT_CONVERTER = new ArgumentConverter<>(
+                String.class, Column.getTypeDummy(String.class), ColumnParser.STRING_COLUMN_PARSER);
+        public static final ArgumentConverter<Boolean> BOOLEAN_ARGUMENT_CONVERTER = new ArgumentConverter<>(
+                Boolean.class, Column.getTypeDummy(Boolean.class), ColumnParser.BOOLEAN_COLUMN_PARSER);
+
+        public final Class<T> runtimeGenericTypeProvider;
+        public final Class<Column<T>> runtimeGenericColumnTypeProvider;
+        public final ColumnParser<T> columnParser;
+
+        private ArgumentConverter(@NotNull Class<T> runtimeGenericTypeProvider,
+                                  @NotNull Class<Column<T>> runtimeGenericColumnTypeProvider,
+                                  @NotNull ColumnParser<T> columnParser) {
+            this.runtimeGenericTypeProvider = Objects.requireNonNull(runtimeGenericTypeProvider);
+            this.runtimeGenericColumnTypeProvider = Objects.requireNonNull(runtimeGenericColumnTypeProvider);
+            this.columnParser = Objects.requireNonNull(columnParser);
+        }
+
+        @NotNull
+        private Optional<String> convertAsColumnArgument(QueryGenerator queryGenerator, @NotNull Object argument) {
+            String convertedArgument;
+            if (runtimeGenericColumnTypeProvider.isAssignableFrom(argument.getClass())) {
+                convertedArgument = queryGenerator.quoteIdentifier(
+                        runtimeGenericColumnTypeProvider.cast(argument).getName());
+            } else {
+                convertedArgument = null;
+            }
+            return Optional.ofNullable(convertedArgument);
+        }
+
+        @NotNull
+        private Optional<String> convertAsTypeArgument(@NotNull Object argument) {
+            String convertedArgument;
+            if (runtimeGenericTypeProvider.isAssignableFrom(argument.getClass())) {
+                convertedArgument = columnParser.toString(runtimeGenericTypeProvider.cast(argument));
+            } else {
+                convertedArgument = null;
+            }
+            return Optional.ofNullable(convertedArgument);
+        }
+
+        @NotNull
+        protected String convertArgument(@NotNull QueryGenerator queryGenerator, @NotNull Object argument) {
+            return convertAsTypeArgument(argument)
+                    .or(() -> convertAsColumnArgument(queryGenerator, argument))
+                    .orElseThrow(() -> new IllegalArgumentException(
+                            String.format("Only objects of type '%s' and '%s' as arguments accepted",
+                                    runtimeGenericTypeProvider.getSimpleName(), Column.class.getSimpleName())));
+        }
+    }
+
+    private static class BinaryQueryCondition<T> extends QueryCondition<T> {
         private final String operator;
 
-        protected BinaryQueryCondition(@NotNull Class<T> runtimeGenericTypeProvider,
-                                       @NotNull Class<Column<T>> runtimeGenericColumnTypeProvider,
-                                       @NotNull ColumnParser<T> columnParser, @NotNull String operator) {
-            super(runtimeGenericTypeProvider, runtimeGenericColumnTypeProvider, columnParser);
+        protected BinaryQueryCondition(
+                @NotNull ArgumentConverter<T> argumentConverter, @NotNull String operator) {
+            super(argumentConverter);
             this.operator = Objects.requireNonNull(operator);
         }
 
         @Override
-        public @NotNull String generateSQLExpression(
+        @NotNull
+        public String generateSQLExpression(
                 @NotNull QueryGenerator queryGenerator, @NotNull Object... arguments) {
             if (arguments.length != 2) {
                 throw new IllegalArgumentException("Exactly two arguments required");
             }
-            String leftHandArgument = convertArgument(queryGenerator, arguments[0]);
-            String rightHandArgument = convertArgument(queryGenerator, arguments[1]);
+            String leftHandArgument = getArgumentConverter()
+                    .convertArgument(queryGenerator, arguments[0]);
+            String rightHandArgument = getArgumentConverter()
+                    .convertArgument(queryGenerator, arguments[1]);
             // FIXME Ensure escaping
             return String.format("%s %s %s", leftHandArgument, operator, rightHandArgument);
         }
     }
 
-    private static class BinaryStringQueryCondition extends BinaryQueryCondition<String> {
-
-        protected BinaryStringQueryCondition(@NotNull String operator) {
-            super(String.class, Column.getTypeDummy(String.class), ColumnParser.STRING_COLUMN_PARSER, operator);
-        }
-    }
-
-    private static abstract class UnaryPrefixQueryCondition<T> extends QueryCondition<T> {
+    private static class UnaryPrefixQueryCondition<T> extends QueryCondition<T> {
         private final String operator;
 
-        protected UnaryPrefixQueryCondition(@NotNull Class<T> runtimeGenericTypeProvider,
-                                            @NotNull Class<Column<T>> runtimeGenericColumnTypeProvider,
-                                            @NotNull ColumnParser<T> columnParser, @NotNull String operator) {
-            super(runtimeGenericTypeProvider, runtimeGenericColumnTypeProvider, columnParser);
+        protected UnaryPrefixQueryCondition(@NotNull ArgumentConverter<T> argumentConverter, @NotNull String operator) {
+            super(argumentConverter);
             this.operator = Objects.requireNonNull(operator);
         }
 
@@ -109,15 +122,10 @@ public abstract class QueryCondition<T> {
             if (arguments.length != 1) {
                 throw new IllegalArgumentException("Exactly one argument required");
             }
-            String argument = convertArgument(queryGenerator, arguments[0]);
+            String argument = getArgumentConverter()
+                    .convertArgument(queryGenerator, arguments[0]);
             // FIXME Ensure escaping
             return String.format("%s %s", operator, argument);
-        }
-    }
-
-    private static class UnaryPrefixBooleanQueryCondition extends UnaryPrefixQueryCondition<Boolean> {
-        protected UnaryPrefixBooleanQueryCondition(@NotNull String operator) {
-            super(Boolean.class, Column.getTypeDummy(Boolean.class), ColumnParser.BOOLEAN_COLUMN_PARSER, operator);
         }
     }
 }
