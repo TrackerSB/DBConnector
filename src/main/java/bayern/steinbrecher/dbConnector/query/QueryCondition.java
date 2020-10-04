797aabb499f997119bdd0d71bc90f6cafceffcd8
Stefan Huber <stefan.huber.niedling@outlook.com>
2020-10-04 11:48:21 +0200

    Reuse existing column parsers and abstract types as far as possible

--- src/main/java/bayern/steinbrecher/dbConnector/query/QueryCondition.java
+++ src/main/java/bayern/steinbrecher/dbConnector/query/QueryCondition.java
@@ -1,9 +1,11 @@
 package bayern.steinbrecher.dbConnector.query;
 
 import bayern.steinbrecher.dbConnector.DBConnection.Column;
+import bayern.steinbrecher.dbConnector.scheme.ColumnParser;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
+import java.util.Optional;
 
 /**
  * @author Stefan Huber
@@ -12,34 +14,61 @@ import java.util.Objects;
 public abstract class QueryCondition<T> {
     public static final QueryCondition<String> LIKE = new BinaryStringQueryCondition("LIKE");
 
+    private final Class<T> runtimeGenericTypeProvider;
+    private final Class<Column<T>> runtimeGenericColumnTypeProvider;
+    private final ColumnParser<T> columnParser;
+
+    protected QueryCondition(@NotNull Class<T> runtimeGenericTypeProvider,
+                             @NotNull Class<Column<T>> runtimeGenericColumnTypeProvider,
+                             @NotNull ColumnParser<T> columnParser) {
+        this.runtimeGenericTypeProvider = Objects.requireNonNull(runtimeGenericTypeProvider);
+        this.runtimeGenericColumnTypeProvider = Objects.requireNonNull(runtimeGenericColumnTypeProvider);
+        this.columnParser = Objects.requireNonNull(columnParser);
+    }
+
+    @NotNull
+    protected Optional<String> convertAsColumnArgument(QueryGenerator queryGenerator, @NotNull Object argument) {
+        String convertedArgument;
+        if (runtimeGenericColumnTypeProvider.isAssignableFrom(argument.getClass())) {
+            convertedArgument = queryGenerator.quoteIdentifier(
+                    runtimeGenericColumnTypeProvider.cast(argument).getName());
+        } else {
+            convertedArgument = null;
+        }
+        return Optional.ofNullable(convertedArgument);
+    }
+
+    @NotNull
+    protected Optional<String> convertAsTypeArgument(@NotNull Object argument) {
+        String convertedArgument;
+        if (runtimeGenericTypeProvider.isAssignableFrom(argument.getClass())) {
+            convertedArgument = columnParser.toString(runtimeGenericTypeProvider.cast(argument));
+        } else {
+            convertedArgument = null;
+        }
+        return Optional.ofNullable(convertedArgument);
+    }
+
+    @NotNull
+    protected String convertArgument(@NotNull QueryGenerator queryGenerator, @NotNull Object argument) {
+        return convertAsTypeArgument(argument)
+                .or(() -> convertAsColumnArgument(queryGenerator, argument))
+                .orElseThrow(() -> new IllegalArgumentException(
+                        String.format("Only objects of type '%s' and '%s' as arguments accepted",
+                                runtimeGenericTypeProvider.getSimpleName(), Column.class.getSimpleName())));
+    }
+
     @NotNull
     public abstract String generateSQLExpression(@NotNull QueryGenerator queryGenerator, @NotNull Object... arguments);
 
     private static abstract class BinaryQueryCondition<T> extends QueryCondition<T> {
-        private final Class<T> runtimeGenericTypeProvider;
-        private final Class<Column<T>> runtimeGenericColumnTypeProvider;
         private final String operator;
 
         protected BinaryQueryCondition(@NotNull Class<T> runtimeGenericTypeProvider,
                                        @NotNull Class<Column<T>> runtimeGenericColumnTypeProvider,
-                                       @NotNull String operator) {
-            this.runtimeGenericTypeProvider = Objects.requireNonNull(runtimeGenericTypeProvider);
-            this.runtimeGenericColumnTypeProvider = Objects.requireNonNull(runtimeGenericColumnTypeProvider);
+                                       @NotNull ColumnParser<T> columnParser, @NotNull String operator) {
+            super(runtimeGenericTypeProvider, runtimeGenericColumnTypeProvider, columnParser);
             this.operator = Objects.requireNonNull(operator);
-        }
-
-        protected abstract String convertArgumentImpl(T argument);
-
-        protected String convertArgument(QueryGenerator queryGenerator, Object argument) {
-            if (runtimeGenericTypeProvider.isAssignableFrom(argument.getClass())) {
-                return convertArgumentImpl(runtimeGenericTypeProvider.cast(argument));
-            }
-            if (runtimeGenericColumnTypeProvider.isAssignableFrom(argument.getClass())) {
-                return queryGenerator.quoteIdentifier(runtimeGenericColumnTypeProvider.cast(argument).getName());
-            }
-            throw new IllegalArgumentException(
-                    String.format("Only objects of type '%s' and '%s' as arguments accepted",
-                            runtimeGenericTypeProvider.getSimpleName(), Column.class.getSimpleName()));
         }
 
         @Override
@@ -58,12 +87,7 @@ public abstract class QueryCondition<T> {
     private static class BinaryStringQueryCondition extends BinaryQueryCondition<String> {
 
         protected BinaryStringQueryCondition(@NotNull String operator) {
-            super(String.class, Column.getTypeDummy(String.class), operator);
-        }
-
-        @Override
-        protected String convertArgumentImpl(String argument) {
-            return String.format("'%s'", argument);
+            super(String.class, Column.getTypeDummy(String.class), ColumnParser.STRING_COLUMN_PARSER, operator);
         }
     }
 }
