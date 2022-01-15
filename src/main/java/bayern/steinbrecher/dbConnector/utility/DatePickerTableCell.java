1691946292075c6dc84b9c7034e32a11a613b35a
Stefan Huber <stefan.huber.niedling@outlook.com>
2022-01-15 16:45:04 +0100

    DatePickerTableCell: Show only while editing and if value present

--- src/main/java/bayern/steinbrecher/dbConnector/utility/DatePickerTableCell.java
+++ src/main/java/bayern/steinbrecher/dbConnector/utility/DatePickerTableCell.java
@@ -5,6 +5,7 @@ import javafx.beans.property.Property;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableCell;
 import javafx.util.Callback;
+import javafx.util.converter.LocalDateStringConverter;
 import org.jetbrains.annotations.NotNull;
 
 import java.time.LocalDate;
@@ -14,7 +15,9 @@ import java.time.LocalDate;
  * @since 0.16
  */
 public class DatePickerTableCell<E, C> extends TableCell<E, C> {
+    private static final LocalDateStringConverter CONVERTER = new LocalDateStringConverter();
     private final Callback<Integer, Property<LocalDate>> getSelectedPropertyCallback;
+    private final DatePicker datePicker = new DatePicker();
 
     public DatePickerTableCell(@NotNull Callback<Integer, Property<LocalDate>> getSelectedPropertyCallback) {
         this.getSelectedPropertyCallback = getSelectedPropertyCallback;
@@ -24,23 +27,43 @@ public class DatePickerTableCell<E, C> extends TableCell<E, C> {
         return getSelectedPropertyCallback.call(getIndex());
     }
 
+    private void bindTextToDate(){
+        textProperty().bind(
+                Bindings.createStringBinding(
+                        () -> CONVERTER.toString(getSelectedProperty().getValue()), getSelectedProperty()));
+    }
+
+    @Override
+    public void startEdit() {
+        if (isEditing()) {
+            // Editing already started
+            return;
+        }
+
+        super.startEdit();
+
+        // Editing started successfully
+        if (isEditing()) {
+            datePicker.valueProperty().bindBidirectional(getSelectedProperty());
+            textProperty().unbind();
+            setText(null);
+            setGraphic(datePicker);
+        }
+    }
+
+    @Override
+    public void cancelEdit() {
+        super.cancelEdit();
+        bindTextToDate();
+        setGraphic(null);
+    }
+
     @Override
     protected void updateItem(C item, boolean empty) {
         super.updateItem(item, empty);
 
-        if (empty) {
-            setText(null);
-            setGraphic(null);
-        } else {
-            var datePicker = new DatePicker();
-            datePicker.valueProperty().bindBidirectional(getSelectedProperty());
-            datePicker.disableProperty().bind(
-                    Bindings.not(
-                            getTableView().editableProperty()
-                                    .and(getTableColumn().editableProperty())
-                                    .and(editableProperty())
-                    ));
-            setGraphic(datePicker);
+        if (!empty && !isEditing()) {
+            bindTextToDate();
         }
     }
 }
